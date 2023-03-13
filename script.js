fdfe1feb6a90040ebcf43718447168c89e58514f
martinnvillar <martinnnvillar@icloud.com>
2023-03-13 14:52:01 -0300

    Add computerChoice and playerChoice functions

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+// Computer and player choice.
+
+function getComputerChoice() {
+    const choice = [
+        'rock', 
+        'paper', 
+        'scissors'
+    ];
+    const randomItem = Math.floor(Math.random() * choice.length);
+    const randChoice = choice[randomItem];
+    console.log(randChoice);
+}
+
+function playerChoice () {
+    const choice = [
+        'rock',
+        'paper',
+        'scissors'
+    ]
+    let choice1 = prompt("Choose paper, rock or scissors: ");
+    console.log(choice1);
+}
+
+
