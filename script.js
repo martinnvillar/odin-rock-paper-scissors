5c1c314c2d2f11861a6fa4abcb601ae0f006cf66
martinnvillar <martinnnvillar@icloud.com>
2023-03-13 15:35:14 -0300

    Add singleRound case insensitive function

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function getComputerChoice() {
     ];
     const randomItem = Math.floor(Math.random() * choice.length);
     const randChoice = choice[randomItem];
-    console.log(randChoice);
 }
 
 function playerChoice () {
@@ -20,5 +19,37 @@ function playerChoice () {
     let choice1 = prompt("Choose paper, rock or scissors: ");
     console.log(choice1);
 }
+
+// Single round
+
+function singleRound (computerSelection, playerSelection) {
+
+    playerSelection = playerSelection.toLowerCase();
+
+    computerSelection = computerSelection.toLowerCase();
+
+    if (computerSelection === playerSelection) {
+        return ("It's a draw!");
+    }
+    else if (computerSelection === "paper" && playerSelection === "rock") {
+        return ("You lose! paper beats rock.");
+    }
+    else if (computerSelection === "scissors" && playerSelection === "paper"){
+        return ("You lose! scissors beats paper. ");
+    }
+    else if (computerSelection === "rock" && playerSelection === "scissors") {
+        return ("You lose! rock beats scissors.");
+    }
+    else if (computerSelection === "rock" && playerSelection === "paper") {
+        return ("You win! paper beats rock.");
+    }
+    else if (computerSelection === "paper" && playerSelection === "scissors") {
+        return ("You win! scissors beats paper.");
+    }
+    else if (computerSelection === "scissors" && playerSelection === "rock") {
+        return ("You win! rock beats scissors.");
+    }
+}
+
 
 
