4617d9f041d30740fb5cfefcbae90c257edc7baf
martinnvillar <martinnnvillar@icloud.com>
2023-04-26 22:26:33 -0300

    add some html and css elements

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const gameOptions = ["rock", "paper", "scissors"];
 
 // random computer choice function
 
-function getComputerChoice () {
-    const randomItem = Math.floor(Math.random() * gameOptions.length);
-    const randChoice = gameOptions[randomItem];
-    return randChoice;
+function getComputerChoice() {
+  const randomItem = Math.floor(Math.random() * gameOptions.length);
+  const randChoice = gameOptions[randomItem];
+  return randChoice;
 }
 
 // round counting variables
@@ -20,53 +20,53 @@ let ties = 0;
 
 // single game function
 
-function playRound (playerChoice, computerChoice) {
-    if (playerChoice === computerChoice) {
-        alert("It's a tie!");
-        ties++;
-    } else if (playerChoice === "paper" && computerChoice === "rock") {
-        alert("You win! paper beats rock.");
-        playerWins++;
-    } else if (playerChoice === "rock" && computerChoice === "scissors") {
-        alert("You win! rock beats scissors.");
-        playerWins++;
-    } else if (playerChoice === "scissors" && computerChoice === "paper") {
-        alert("You win! scissors beats paper");
-        playerWins++
-    } else if (playerChoice === "rock" && computerChoice === "paper") {
-        alert("You lose! paper beats rock.");
-        computerWins++;
-    } else if (playerChoice === "scissors" && computerChoice === "rock") {
-        alert("You lose! rock beats scissors.");
-        computerWins++;
-    } else if (playerChoice === "paper" && computerChoice === "scissors") {
-        alert("You lose! scissors beats paper.");
-        computerWins++;
-    }
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    alert("It's a tie!");
+    ties++;
+  } else if (playerChoice === "paper" && computerChoice === "rock") {
+    alert("You win! paper beats rock.");
+    playerWins++;
+  } else if (playerChoice === "rock" && computerChoice === "scissors") {
+    alert("You win! rock beats scissors.");
+    playerWins++;
+  } else if (playerChoice === "scissors" && computerChoice === "paper") {
+    alert("You win! scissors beats paper");
+    playerWins++;
+  } else if (playerChoice === "rock" && computerChoice === "paper") {
+    alert("You lose! paper beats rock.");
+    computerWins++;
+  } else if (playerChoice === "scissors" && computerChoice === "rock") {
+    alert("You lose! rock beats scissors.");
+    computerWins++;
+  } else if (playerChoice === "paper" && computerChoice === "scissors") {
+    alert("You lose! scissors beats paper.");
+    computerWins++;
+  }
 }
 
 // five round game
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let computerChoice = getComputerChoice();
-        let playerChoice = prompt("Choose rock, paper or scissors: ");
-        playerChoice = playerChoice.toLocaleLowerCase();
-        if (playerChoice == "") {
-            alert("You should write rock, paper or scissors!")
-        } else {
-            playRound(playerChoice, computerChoice);
-            round++;
-            console.log("This is the round number " + round);
-        }
-    }
-    if (playerWins > computerWins) {
-        alert("You WIN!");
-    } else if (playerWins === computerWins) {
-        alert("Game TIE!");
-    } else {
-        alert("Computer BEATS you!");
-    }
-}
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         let computerChoice = getComputerChoice();
+//         let playerChoice = prompt("Choose rock, paper or scissors: ");
+//         playerChoice = playerChoice.toLocaleLowerCase();
+//         if (playerChoice == "") {
+//             alert("You should write rock, paper or scissors!")
+//         } else {
+//             playRound(playerChoice, computerChoice);
+//             round++;
+//             console.log("This is the round number " + round);
+//         }
+//     }
+//     if (playerWins > computerWins) {
+//         alert("You WIN!");
+//     } else if (playerWins === computerWins) {
+//         alert("Game TIE!");
+//     } else {
+//         alert("Computer BEATS you!");
+//     }
+// }
 
-game();
+// game();
